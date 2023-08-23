bcd64d84945725668390378c1034c0b43e21e4df
unknown <jahid1604091>
2023-08-23 22:58:27 +0600

    dotnenv set and db connected

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from 'express';
-
+import {connectDB} from './config/db.js';
+import dotenv from 'dotenv';
 
 const app = express();
-
+dotenv.config();
+connectDB();
 
 app.get('/',(req,res)=>{
     res.send('API running...')
